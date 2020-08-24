8cd7e0b4b3e75180cac470f600f871238d7eb82e
Artyom <thomasway7@gmail.com>
2020-08-24 19:00:46 +0300

    modal

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -1,23 +1,44 @@
 // import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 import { ExtContainer, ExtButton } from '@sencha/ext-react-modern';
-
+const Ext = window['Ext'];
 
 class ButtonComponent extends Component {
     constructor(props) {
         super(props);
 
         this.state = { message: null };
+
+
+        // =====
+        var dialog = Ext.create({
+             xtype: 'dialog',
+             title: 'Dialog',
+
+             maximizable: true,
+             html: 'Content<br>goes<br>here',
+
+             buttons: {
+                 ok: function () {  // standard button (see below)
+                     dialog.destroy();
+                 }
+             }
+         });
+        dialog.show();
+    
+         
     }
 
     render() {
-        // handler={this.onButtonTap.bind(this)}
+        
         // onClick={this.props.func}
+        // handler={this.props.func}
         return (
             <ExtContainer padding="10">
                 <ExtButton
                     text="My Button"
-                    handler={this.props.func}
+                    
+                    handler={this.onButtonTap.bind(this)}
                     ui="action raised" />
                 <div>
                     {this.state.message}
@@ -28,10 +49,19 @@ class ButtonComponent extends Component {
 
     onButtonTap() {
         // this.setState({ message: 'Tapped!' });
-        // this.props.func;
+        // this.props.func
+        
+        // this.props.dialog.show();
     }
+    componentDidMount() {
+        console.log(this.props);
+      } 
+
+
+    
     
 }
+
 
 // const Ext = window['Ext'];
 // Ext.onReady(() => {
--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+
 import { ExtGrid } from "@sencha/ext-react-modern";
 import { ExtColumn } from "@sencha/ext-react-modern";
 import ButtonComponent from './ButtonComponent';
+
 const Ext = window['Ext'];
 
 
@@ -14,121 +16,101 @@ class TableComponent extends Component {
       isLoaded: false,
       items: []
     };
-    // var data=[
-    //   { id: 1, name: 'Kolya',val3: .25 },
-    //   { id: 2, name: 'Vsevolod',val3: .35 },
-    //   { id: 3, name: 'Dima',val3: .45 },
-    //   { id: 4, name: 'Anton',val3: .45 }
-    // ]
-    // this.store = Ext.create('Ext.data.Store', {data: data});
-    //this.store = {xtype: 'store',data: data}
-
+    
     Ext.define('User', {
          extend: 'Ext.data.Model',
          fields: [
              {name: 'id',    type: 'int'},
              {name: 'name',  type: 'string'},
-             {name: 'val3',  type: 'float'}
+             {name: 'email',  type: 'string'},
+             {name: 'phone', type: 'string'}, 
+             {name: 'password', type: 'string'},
+             {name: 'role_id', type: 'string'},
+             {name: 'parent_name', type: 'string'},
+             {name: 'parent_phone', type: 'string'},
+             {name: 'parent_email', type: 'string'},
+             {name: 'registered_as', type: 'string'},
+             {name: 'age', type: 'string'},
+             {name: 'gender', type: 'string'}
          ]
      });
 
     this.store = Ext.create('Ext.data.Store', {
+        pageSize: 3,
         model: 'User',
         proxy: {
             type: 'ajax',
-            url: 'https://run.mocky.io/v3/c1e16bd6-51bb-41d8-a1ad-efe3702f2801',
+            url: 'https://run.mocky.io/v3/a0b277c9-d52f-4519-9982-64933c5052eb',
             reader: {
                 type: 'json',
                 rootProperty: 'items'
             }
         },
-        // autoLoad: true
+        
+        autoLoad: true
     });
 
-    // this.output = this.output.bind(this); //не понимаю, зачем
+    Ext.require('Ext.grid.plugin.PagingToolbar');
+ 
   }
 
-  output() {
-          // console.log("With love from parent P land :)");
-          // this.loadItemsToStore();
-          
-      }
 
-  loadItemsToStore(){
-    this.store.load({
-        params: {
-            group: 3,
-            type: 'user'
-        },
-        callback: function(records, operation, success) {
-            // do something after the load finishes
-        },
-        scope: this
-    });
+
+  output(){
+    // console.log("gg");
   }
-
-  
-  componentDidMount() { 
-    this.loadItemsToStore();
-
-    fetch("https://run.mocky.io/v3/c1e16bd6-51bb-41d8-a1ad-efe3702f2801")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.items
-          });
-          // console.log(this.store);
-          // console.log(result.items);
-          // console.log(this.state);
-          // this.data = result.items;
-        },
-        // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
-        // чтобы не перехватывать исключения из ошибок в самих компонентах.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-  //new
-
   extReactDidMount = detail => {
-    // console.log('extReactDidMount')
+    // console.log(detail)
     // this.grid.cmp.setStore(this.store);
-    // this.store.add({some: 'data'}, {some: 'other data'});
+    // this.store.add({ "id": 12, "name": "Kolya", "email": "artyom@mail.ru", "phone":"89111950123","password":"123","role_id":"user","parent_name":"Artyom","parent_phone":"89110011222","parent_email":"papa@mail.ru","registered_as":"parent","age":"21","gender":"male"});
+
   }
 
-  renderSign = (format, value) => (
+
+  renderSign = (format, value) => (  
     <span style={{ color: value > 0 ? 'green' : value < 0 ? 'red' : ''}}>
         {Ext.util.Format.number(value, format)}
     </span>
   )
 
+
+
   render() {
     return (
      <React.Fragment>
-      <ButtonComponent func={this.output}/>
+      
       <ExtGrid
         viewport={ true }
         ref={ grid => this.grid = grid }
-        title="Табличка"
+        title="Пользователи"
         store={ this.store }
         onReady={ this.extReactDidMount }
+        plugins={['pagingtoolbar']}
+
+
       >
-        <ExtColumn text="id" dataIndex="id"></ExtColumn>
-        <ExtColumn text="name" dataIndex="name" width="150"></ExtColumn>
-        <ExtColumn
-          text="val3"
-          dataIndex="val3"
-          align="right"
-          renderer={ this.renderSign.bind(this, '0.00') }
-        />
+        
+        <ExtColumn text="link" dataIndex="id" encodeHtml="false" 
+        // renderer={function(v) {return <a href="https://edu.advance24.online/user/id='+ v.toString() +'">Ссылка</a>;}}       
+        renderer={function(v) {return <ButtonComponent func={v}/>;}}
+
+        
+
+        
+        ></ExtColumn>
+        <ExtColumn flex={1} text="name" dataIndex="name" width="150"></ExtColumn>
+        <ExtColumn flex={1} text="email" dataIndex="email" width="150"></ExtColumn>
+        <ExtColumn flex={1} text="phone" dataIndex="phone" width="150"></ExtColumn>
+        <ExtColumn flex={1} text="password" dataIndex="password" width="150"></ExtColumn>
+        <ExtColumn flex={1} text="role_id" dataIndex="role_id" width="150"></ExtColumn>
+        <ExtColumn flex={1} text="parent_name" dataIndex="parent_name" width="150"></ExtColumn>
+        <ExtColumn flex={1} text="parent_phone" dataIndex="parent_phone" width="150"></ExtColumn>
+        <ExtColumn flex={1} text="parent_email" dataIndex="parent_email" width="150"></ExtColumn>
+        <ExtColumn flex={1} text="registered_as" dataIndex="registered_as" width="150"></ExtColumn>
+        <ExtColumn flex={1} text="age" dataIndex="age" width="150"></ExtColumn>
+        <ExtColumn flex={1} text="gender" dataIndex="gender" width="150"></ExtColumn>
       </ExtGrid>
-     </React.Fragment>
+    </React.Fragment>
     )
   }
 
